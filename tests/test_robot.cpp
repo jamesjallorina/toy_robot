1d49e2b077a6ff7ac7757606cc1be1ed2ae6be5e
jamesjallorina <jamesmarjun.jallorina@yahoo.com.ph>
2022-01-31 03:51:02 +0800

    add: counter-clockwise square movement robot test
    Signed-off-by: jamesjallorina <jamesmarjun.jallorina@yahoo.com.ph>

--- tests/test_robot.cpp
+++ tests/test_robot.cpp
@@ -120,5 +120,92 @@ TEST_CASE("square movement", "[Robot]")
 /// @brief Robot moves from East -> North -> West -> South
 TEST_CASE("reverse square movement", "[Robot]")
 {
-
+    Coordinates c = {0, 0, Face::East};
+    Robot r;
+    // move 4 steps to north, coordinate should 4, 0, East
+    r.place(c);
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+    {
+        const auto current_coordinates = r.get_state();
+        CHECK(current_coordinates.x_pos == 4);
+        CHECK(current_coordinates.y_pos == 0);
+        CHECK(current_coordinates.f_pos == Face::East);
+    }
+    // rotate left, cooridinate should be 4, 0, North
+    r.rotate(Rotate::Left);
+    {
+        const auto current_coordinates = r.get_state();
+        CHECK(current_coordinates.x_pos == 4);
+        CHECK(current_coordinates.y_pos == 0);
+        CHECK(current_coordinates.f_pos == Face::North);
+    }
+    // move 4 steps to north, cooridate should be 4, 4, North
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+    {
+        const auto current_coordinates = r.get_state();
+        CHECK(current_coordinates.x_pos == 4);
+        CHECK(current_coordinates.y_pos == 4);
+        CHECK(current_coordinates.f_pos == Face::North);
+    }
+    // rotate left, coordinate should be 4, 4, East
+    r.rotate(Rotate::Left);
+    {
+        const auto current_coordinates = r.get_state();
+        CHECK(current_coordinates.x_pos == 4);
+        CHECK(current_coordinates.y_pos == 4);
+        CHECK(current_coordinates.f_pos == Face::West);
+    }
+    // move 4 steps East, coordinate should be 0, 4, East
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+    {
+        const auto current_coordinates = r.get_state();
+        CHECK(current_coordinates.x_pos == 0);
+        CHECK(current_coordinates.y_pos == 4);
+        CHECK(current_coordinates.f_pos == Face::West);
+    }
+    // rotate left, coordinate should be 4, 0, South
+    r.rotate(Rotate::Left);
+    {
+        const auto current_coordinates = r.get_state();
+        CHECK(current_coordinates.x_pos == 0);
+        CHECK(current_coordinates.y_pos == 4);
+        CHECK(current_coordinates.f_pos == Face::South);
+    }
+    // move 4 steps South, coordinate should be 0, 0, South
+    r.move();
+    r.move();
+    r.move();
+    r.move();
+    {
+        const auto current_coordinates = r.get_state();
+        CHECK(current_coordinates.x_pos == 0);
+        CHECK(current_coordinates.y_pos == 0);
+        CHECK(current_coordinates.f_pos == Face::South);
+    }
+    // get back to original face position
+    // rotate Left, coordinate should be 0, 0, East
+    r.rotate(Rotate::Left);
+    {
+        const auto current_coordinates = r.get_state();
+        CHECK(current_coordinates.x_pos == 0);
+        CHECK(current_coordinates.y_pos == 0);
+        CHECK(current_coordinates.f_pos == Face::East);
+    }
+    // rotate Left, coordinate should be 0, 0, North
+    r.rotate(Rotate::Left);
+    {
+        const auto current_coordinates = r.get_state();
+        CHECK(current_coordinates.x_pos == 0);
+        CHECK(current_coordinates.y_pos == 0);
+        CHECK(current_coordinates.f_pos == Face::North);
+    }
 }
