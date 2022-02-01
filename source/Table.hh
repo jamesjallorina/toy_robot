54dc98a186a0f22f7188cc77d96c9340e4cb0c75
jamesjallorina <jamesmarjun.jallorina@yahoo.com.ph>
2022-02-01 13:22:16 +0800

    add: api to table class
    Signed-off-by: jamesjallorina <jamesmarjun.jallorina@yahoo.com.ph>

--- source/Table.hh
+++ source/Table.hh
@@ -8,6 +8,11 @@
 // currently supports only 1 robot as stated in the requirements
 constexpr uint8_t unique_robot_index = 0;
 
+// default values for x and y table dimensions
+constexpr int default_x_dim = 5;
+constexpr int default_y_dim = 5;
+constexpr size_t default_robot_size = 1;
+
 // The Table class is responsible for the following 
 // 1. handle collision detection
 // 2. prevent robots from falling
@@ -27,10 +32,14 @@ public:
     bool process_command(UserInput const &input, 
             const uint8_t robot_id = unique_robot_index);
 
+    int get_x_dim() const { return m_dim_x; }
+    int get_y_dim() const { return m_dim_y; }
+    size_t get_robot_size() const { return robots.size(); }
+
 private:
-    int m_dim_x;
-    int m_dim_y;
-    std::vector<Robot> robots;
+    int m_dim_x = default_x_dim;
+    int m_dim_y = default_y_dim;
+    std::vector<Robot> robots{default_robot_size};
 };
 
 #endif  // TABLE_HH
